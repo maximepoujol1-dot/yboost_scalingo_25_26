425712121139ffec9a6dd71d92fff24c4abe8a62
Maxime <maximepoujol1@gmail.com>
2026-01-28 16:14:52 +0100

    procfile

--- Api/filter.go
+++ Api/filter.go
@@ -1,1 +1,80 @@
 package api
+
+//check and change the thuth table
+
+func Verifswitch(verifi map[string]bool, motsVerif string) {
+
+    switch motsVerif {
+    case "verifAlbumDate":
+        if verifi["verifAlbumDate"] == true {
+            verifi["verifAlbumDate"] = false
+        } else {
+            verifi["verifAlbumDate"] = true
+        }
+	
+
+    case "verifDate":
+        if verifi["verifDate"] == true {
+            verifi["verifDate"] = false
+        } else {
+            verifi["verifDate"] = true
+        }
+	
+
+    case "verifMemberNumber":
+        if verifi["verifMemberNumber"] == true {
+            verifi["verifMemberNumber"] = false
+        } else {
+            verifi["verifMemberNumber"] = true
+        }
+
+	case "verifLocation":
+        if verifi["verifLocation"] == true {
+            verifi["verifLocation"] = false
+        } else {
+            verifi["verifLocation"] = true
+        }
+
+
+
+	case "réinitialiser":
+        if verifi["réinitialiser"] == true {
+            verifi["réinitialiser"] = false
+        } else {
+            verifi["réinitialiser"] = true
+        }
+	
+
+
+
+    case "verifNonAlbumDate":
+        if verifi["verifNonAlbumDate"] == true {
+            verifi["verifNonAlbumDate"] = false
+        } else {
+            verifi["verifNonAlbumDate"] = true
+        }
+	
+
+    case "verifNonDate":
+        if verifi["verifNonDate"] == true {
+            verifi["verifNonDate"] = false
+        } else {
+            verifi["verifNonDate"] = true
+        }
+	
+
+    case "verifNonMemberNumber":
+        if verifi["verifNonMemberNumber"] == true {
+            verifi["verifNonMemberNumber"] = false
+        } else {
+            verifi["verifNonMemberNumber"] = true
+        }
+
+	case "verifNonLocation":
+        if verifi["verifNonLocation"] == true {
+            verifi["verifNonLocation"] = false
+        } else {
+            verifi["verifNonLocation"] = true
+        }
+    }
+}
--- Api/viking.go
+++ Api/viking.go
@@ -1,1 +1,68 @@
 package api
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+//get the API of all artist
+
+func ChooseArtisteAll() ([]byte, error) {
+
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	response, err := http.Get(url)
+
+	if err != nil {
+
+		fmt.Println("Erreur lors de la requête HTTP :", err)
+
+		return nil, err
+
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+
+		fmt.Println("Erreur lors de la lecture de la réponse :", err)
+
+		return nil, err
+
+	}
+
+	return body, nil
+}
+
+//get the API of just one artist
+
+func ChooseArtistePrecise(id string) ([]byte, error) {
+
+	url := "https://groupietrackers.herokuapp.com/api/artists/" + id
+	response, err := http.Get(url)
+
+	if err != nil {
+
+		fmt.Println("Erreur lors de la requête HTTP :", err)
+
+		return nil, err
+
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+
+		fmt.Println("Erreur lors de la lecture de la réponse :", err)
+
+		return nil, err
+
+	}
+
+	return body, nil
+
+}

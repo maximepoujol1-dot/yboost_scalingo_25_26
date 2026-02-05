982322a3c5e62bb588be2636922f3e5cb659abad
Maxime <maximepoujol1@gmail.com>
2026-02-05 21:04:36 +0100

    correction

--- Api/viking.go
+++ Api/viking.go
@@ -35,34 +35,3 @@ func ChooseArtisteAll() ([]byte, error) {
 
 	return body, nil
 }
-
-//get the API of just one artist
-
-func ChooseArtistePrecise(id string) ([]byte, error) {
-
-	url := "https://apiyboostviking.osc-fr1.scalingo.io/data/viking.json/" + id
-	response, err := http.Get(url)
-
-	if err != nil {
-
-		fmt.Println("Erreur lors de la requête HTTP :", err)
-
-		return nil, err
-
-	}
-
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-
-		fmt.Println("Erreur lors de la lecture de la réponse :", err)
-
-		return nil, err
-
-	}
-
-	return body, nil
-
-}

3e77f18f7662619676c52b48e22d8eec729bc689
Maxime <maximepoujol1@gmail.com>
2026-02-05 19:19:50 +0100

    correction

--- main.go
+++ main.go
@@ -63,9 +63,10 @@ func main() {
 	http.HandleFunc("/aboutus", homeHandler3)
 	http.HandleFunc("/filtre", homeHandler4)
 
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8081" 
+		port = "8081" // pour ton local seulement
 	}
 
 	fmt.Println("Server started at :" + port)
@@ -75,6 +76,7 @@ func main() {
 		fmt.Println("Server error:", err)
 	}
 }
+
 
 
 // redirect to the good page
